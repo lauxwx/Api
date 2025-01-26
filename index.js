dc97e2b7ed8b00271d6c36cbeac944e23a6bae71
lauxwx <pineda.solon.laura@gmail.com>
2025-01-26 17:24:30 -0500

    arreglar puerto

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/", recetaRoutes);
 
 // Solo iniciar el servidor si está en entorno local
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 10000;
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
   });
